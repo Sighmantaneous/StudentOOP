c29e62f56a8027eacd83a02235f4c3fc9149718f
Sighman <g00286398@atu.ie>
2024-10-15 20:47:48 +0100

    Updating to make inputs use just one method to store info and return a new instance of Student class

--- src/main/java/StudentApp.java
+++ src/main/java/StudentApp.java
@@ -2,47 +2,29 @@ import java.util.Scanner;
 
 public class StudentApp {
     public static void main(String[] args) {
-        String userName,email,course;
-        String userName2,email2,course2;
-        String userName3,email3, course3;
-
-        System.out.println("Please enter the name of the student");
         Scanner scanner = new Scanner(System.in);
-        userName=scanner.next();
-        System.out.println("Please enter the email of the student");
-        email=scanner.next();
-        System.out.println("Please enter the course of the student");
-        course=scanner.next();
-
-        System.out.println("Please enter the name of the student");
-        userName2=scanner.next();
-        System.out.println("Please enter the email of the student");
-        email2=scanner.next();
-        System.out.println("Please enter the course of the student");
-        course2=scanner.next();
-
-        System.out.println("Please enter the name of the student");
-        userName3=scanner.next();
-        System.out.println("Please enter the email of the student");
-        email3=scanner.next();
-        System.out.println("Please enter the course of the student");
-        course3=scanner.next();
 
 
+        Student student = getStudentInfo(scanner);
+        Student student2 = getStudentInfo(scanner);
+        Student student3 = getStudentInfo(scanner);
 
 
-        Student student = new Student();
-        student.setName(userName);
-        student.setEmail(email);
-        student.setCourse(course);
         System.out.println(student.toString());
-
-        Student student2 = new Student(userName2);
-        student2.setEmail(email2);
-        student2.setCourse(course2);
         System.out.println(student2.toString());
-
-        Student student3 = new Student(userName3,email3,course3);
         System.out.println(student3.toString());
         }
+
+    public static Student getStudentInfo(Scanner scanner) {
+        System.out.println("Please enter the name of the student");
+        String name = scanner.next();
+        System.out.println("Please enter the email of the student");
+        String email = scanner.next();
+        System.out.println("Please enter the course of the student");
+        String course = scanner.next();
+        return new Student(name, email, course);
+
     }
+    }
+
+
